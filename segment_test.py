e9f711188e69685c4a065f8439073889ebfec36f
0vos <liqianqian1872@gmail.com>
2025-10-29 16:48:26 +0800

    更新

---
+++ segment_test.py
@@ -0,0 +1,50 @@
+import sys
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import cv2
+sys.path.append("..")
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+
+sam_checkpoint = "sam_vit_h_4b8939.pth"
+model_type = "vit_h"
+
+device = "cpu"
+
+sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+sam.to(device=device)
+
+mask_generator = SamAutomaticMaskGenerator(sam)
+
+# step 2
+image = cv2.imread('images/IMG_8794.JPG')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# plt.figure(figsize=(20,20))
+# plt.imshow(image)
+# plt.axis('off')
+# plt.show()
+
+masks = mask_generator.generate(image)
+print(len(masks))
+print(masks[0].keys())
+# step 3
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
+# step 4
+plt.figure(figsize=(20,20))
+plt.imshow(image)
+show_anns(masks)
+plt.axis('off')
+plt.show() 
